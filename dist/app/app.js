6611bb92db327a72ba4773f1136818ad1432eebd
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-11-18 18:06:18 +0600

    exploring router in expressjs

--- dist/app/app.js
+++ dist/app/app.js
@@ -8,23 +8,45 @@ const app = (0, express_1.default)();
 //parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: "true",
+        message: "user is created succesfully",
+        data: user,
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: "true",
+        message: "course is created succesfully",
+        data: course,
+    });
+});
 const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
     next();
 };
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/:userId/:subId', logger, (req, res) => {
+app.get("/:userId/:subId", logger, (req, res) => {
     console.log(req.params);
-    res.send('hello developer Rayhan');
+    res.send("hello developer Rayhan");
 });
-app.get('/', logger, (req, res) => {
+app.get("/", logger, (req, res) => {
     console.log(req.query);
-    res.send('hello world!');
+    res.send("hello world!");
 });
 app.post("/", (req, res) => {
     console.log(req.body);
     res.json({
-        message: "succesfully recived data"
+        message: "succesfully recived data",
     });
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,30 +1,52 @@
-import express, { NextFunction, Request, Response } from 'express'
-const app = express()
+import express, { NextFunction, Request, Response } from "express";
+const app = express();
 
 //parsers
-app.use(express.json())
-app.use(express.text())
+app.use(express.json());
+app.use(express.text());
 
-const logger = (req:Request,res:Response,next:NextFunction)=>{
-  console.log(req.url,req.method,req.hostname);
+const userRouter = express.Router();
+const courseRouter = express.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: "true",
+    message: "user is created succesfully",
+    data: user,
+  });
+});
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: "true",
+    message: "course is created succesfully",
+    data: course,
+  });
+});
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
   next();
-}
+};
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/:userId/:subId',logger, (req:Request, res:Response) => {
+app.get("/:userId/:subId", logger, (req: Request, res: Response) => {
   console.log(req.params);
-  res.send('hello developer Rayhan')
-})
-app.get('/',logger, (req:Request, res:Response) => {
+  res.send("hello developer Rayhan");
+});
+app.get("/", logger, (req: Request, res: Response) => {
   console.log(req.query);
-  res.send('hello world!')
-})
+  res.send("hello world!");
+});
 
-app.post("/",(req:Request,res:Response)=>{
+app.post("/", (req: Request, res: Response) => {
   console.log(req.body);
   res.json({
-    message:"succesfully recived data"
-  })
-})
-
+    message: "succesfully recived data",
+  });
+});
 
 export default app;
